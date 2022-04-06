ab9a678f1b300bca9a461c14c658db79599f6701
KRSRIKAN <102959700+KRSRIKAN@users.noreply.github.com>
2022-04-06 10:52:03 +0530

    Add files via upload

---
+++ MyWorld.java
@@ -0,0 +1,15 @@
+package com.cap.day;
+
+public class MyWorld {
+
+	
+		
+		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+		System.out.println("enter the word");
+	}
+		
+
+	}
+
+
